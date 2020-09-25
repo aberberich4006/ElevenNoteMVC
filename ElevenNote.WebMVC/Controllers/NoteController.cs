1d362135d660b24e881f9747d701ed1f34f66b57
Aaron Berberich <aberberich4006@gmail.com>
2020-09-25 02:27:34 -0400

    Added Notes to navbar and user authentication

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ElevenNote.WebMVC.Controllers
+namespace ElevenNote.Web.Controllers
 {
+    [Authorize]
     public class NoteController : Controller
     {
-        // GET: Note
         public ActionResult Index()
         {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
