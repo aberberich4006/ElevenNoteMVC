109633533e489ab95601ef09c337e7e8213dfeda
Aaron Berberich <aberberich4006@gmail.com>
2020-09-25 03:07:07 -0400

    Added note detail view

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -46,6 +46,13 @@ namespace ElevenNote.Web.Controllers
 
             return View(model);
         }
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
 
         private NoteService CreateNoteService()
         {
